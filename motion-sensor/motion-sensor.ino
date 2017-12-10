3e172ea09424f8763f1a3e6306e9eef9c87f9dc3
Mark Furland <mtfurlan@i3detroit.org>
2017-12-10 02:52:45 -0500

    Made motion sensor generic

--- motion-sensor/motion-sensor.ino
+++ motion-sensor/motion-sensor.ino
@@ -4,6 +4,13 @@
 #include "mqtt-wrapper.h"
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
+#ifndef NAME
+#define NAME "NEW-motion-sensor"
+#endif
+
+#ifndef TOPIC
+#define TOPIC "i3/program-me/NEW-motion-sensor"
+#endif
 
 char buf[1024];
 char topicBuf[1024];
@@ -11,7 +18,8 @@ char topicBuf[1024];
 bool control_state;
 
 
-const char* host_name = "machine-shop-motion-sensor";
+const char* host_name = NAME;
+const char* topic = TOPIC;
 const char* ssid = "i3detroit-wpa";
 const char* password = "i3detroit";
 const char* mqtt_server = "10.13.0.22";
@@ -32,6 +40,9 @@ int motionCooldown = 1000*60;
 int lastPublish = -durationBetweenPublishes;
 int lastMotion = millis();
 
+unsigned long offCheck = 0UL;
+unsigned long offCheckInterval = 60000UL;
+
 void callback(char* topic, byte* payload, unsigned int length, PubSubClient *client) {
 
 }
@@ -40,7 +51,8 @@ void connectSuccess(PubSubClient* client, char* ip) {
   //subscribe and shit here
   //TODO:
   sprintf(buf, "{\"Hostname\":\"%s\", \"IPaddress\":\"%s\"}", host_name, ip);
-  client->publish( "stat/i3/inside/machineShop/motion-sensor/INFO2", buf);
+  sprintf(topicBuf, "stat/%s/INFO2", topic);
+  client->publish(topicBuf, buf);
 }
 
 void setup() {
@@ -53,22 +65,24 @@ void setup() {
 }
 
 void connectedLoop(PubSubClient* client) {
-  //Foreach pin run debouncing, and if it's a real press call the function
+  if( (long)( millis() - offCheck ) >= 0) {
+    offCheck = millis() + offCheckInterval;
+    //Turn off if needed
+    if (millis() - lastMotion > motionCooldown) {
+      sprintf(topicBuf, "stat/%s/motion", topic);
+      client->publish(topicBuf, "OFF");
+    }
+  }
+
   for(int i=0; i < numButtons; ++i) {
-    button_state[i] = digitalRead(button_pins[i]);//Read current state
-    //If the state has changed 
+    button_state[i] = digitalRead(button_pins[i]);
     if (button_state[i] != button_state_last[i] && millis() - debounce[i] > debounce_time) {
-      // AND if it's been long enough since the last state change
-      if(millis() - lastPublish > durationBetweenPublishes && i == 0) {
-        //If there's motion
-        if (button_state[i] == LOW) {
-          client->publish("stat/i3/inside/machineShop/motion-sensor/motion", "ON");
-          lastPublish = millis();
-          lastMotion = lastPublish;
-        }
-        //If there's no motion AND it's been long enough since motion was last detected
-        else if (button_state[i] == HIGH && millis() - lastMotion > motionCooldown) {
-          client->publish("stat/i3/inside/machineShop/motion-sensor/motion", "OFF");
+      //If motion
+      if (button_state[i] == LOW) {
+        lastMotion = millis();
+        if(millis() - lastPublish > durationBetweenPublishes) {
+          sprintf(topicBuf, "stat/%s/motion", topic);
+          client->publish(topicBuf, "ON");
           lastPublish = millis();
         }
       }
